3c5374cf7b1fb971ef3e9ad86be152a698a4168d
Zachary Stevens <zts@cryptocracy.com>
2011-10-19 16:33:40 +0100

    Parameterise logstash download location.

--- attributes/default.rb
+++ attributes/default.rb
@@ -17,6 +17,7 @@
 # limitations under the License.
 #
 
-default['logstash']['version'] = "1.0.14"
-default['logstash']['checksum'] = "06fc4d82dc87d65a581c881b94cb34735bab5186121cd05f3daa5e80f3357807"
+default['logstash']['version'] = "1.0.17"
+default['logstash']['checksum'] = "caba048cb1ab3cc608d4569246f8b7effbb8272865c7864a662566c30517316c"
+default['logstaion']['source_path'] = "http://semicomplete.com/files/logstash/"
 default['logstash']['install_path'] = "/srv/logstash"
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,7 +1,8 @@
 #
 # Cookbook Name:: logstash
-# Recipe:: default
+# Recipe:: server
 #
+# Copyright 2011, Zachary Stevens
 # Copyright 2011, Joshua Timberman
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -24,7 +25,14 @@ directory node['logstash']['install_path'] do
   group "nogroup"
 end
 
-remote_file "#{node['logstash']['install_path']}/logstash-monolithic.jar" do
+cookbook_file "/etc/init.d/logstash" do
+  source "logstash.init"
+  owner  "root"
+  group  "root"
+  mode   "755"
+end
+
+cookbook_file "#{node['logstash']['install_path']}/logstash-monolithic.jar" do
   source "#{node['logstash']['source_path']}/logstash-#{node['logstash']['version']}-monolithic.jar"
   owner "nobody"
   group "nogroup"
